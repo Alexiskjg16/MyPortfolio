9c94da765b678b57192d1ccb0b5c443f16b14125
Alexis Grisham <alexiskjg16@gmail.com>
2018-09-21 10:04:35 -0400

    trying to fix links

--- src/CodingFiles/HomeCoding.js
+++ src/CodingFiles/HomeCoding.js
@@ -13,9 +13,9 @@ class HomeCoding extends Component {
                     <li className="CodeList"> Hey! This whole website is built using REACT.js from scratch. </li>
                     <li className="CodeList"> Scoreboard - <a href="http://faithful-trouble.surge.sh/" target="_blank">Go Jayhawks! </a>
                         -- <a href="https://github.com/Alexiskjg16/VueScoreboard" target="_blank">What's the Code?</a></li>
-                        <li className="CodeList"><a href="http://snowing-hangman.surge.sh" target="_blank"> Hangman </a>
+                        <li className="CodeList"><a href="https://snowing-hangman.surge.sh" target="_blank"> Hangman </a>
                         -- <a href="https://github.com/Alexiskjg16/SnowmanGame" target="_blank">What's the Code?</a></li>
-                        <li className="CodeList"><a href="http://weather-app.alexisgrisham.surge.sh" target="_blank"> WeatherAPI </a>
+                        <li className="CodeList"><a href="https://weather-app.alexisgrisham.surge.sh" target="_blank"> WeatherAPI </a>
                         --<a href="https://github.com/Alexiskjg16/weather-app" target="_blank"> What's the Code?</a></li>
                 </ul>
                 <section>
