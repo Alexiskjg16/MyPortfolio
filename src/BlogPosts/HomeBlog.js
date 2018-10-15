cc80da588e2690fdc64ccb73f487a7d092064a38
Alexis K Grisham <alexisgrisham@Alexiss-MacBook-Air.local>
2018-10-15 17:06:45 -0400

    addedblogimages

--- src/BlogPosts/HomeBlog.js
+++ src/BlogPosts/HomeBlog.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../NavBar';
-import Footer from '../FooterContact'
+import Footer from '../FooterContact';
+import suncoastKids from '../Pictures/suncoastkids.jpg';
+import imposterSyndrome from '../Pictures/imposterSyndrome.jpeg';
+import classroomImage from '../Pictures/classroomImage.jpeg';
 
 
 class HomeBlog extends Component {
@@ -11,11 +14,20 @@ class HomeBlog extends Component {
                 <section>
                     < NavBar />
                 </section>
+                <h1 className="MainBlogTitle"> Blog Posts </h1>
                 <section className="MainBlogPage">
-                <h1> Blog Posts </h1>
+                <section className="bloggerHeads">
+                <img src={suncoastKids} alt="GroupImage" className="blogImages" /> 
                 <Link to={"/BlogPosts/Sept518"} className="BlogLinks"> Being the Only Girl </Link>
+                </section>
+                <section className="bloggerHeads">
+                <img src={imposterSyndrome} alt="imposterSyndromeImage" className="blogImages" />
                 <Link to={"/BlogPosts/Sept1118"} className="BlogLinks"> The Imposter Syndrome </Link>
+                </section>
+                <section className="bloggerHeads">
+                <img src={classroomImage} alt="classroomImage" className="blogImages" />
                 <Link to={"/BlogPosts/Sept2418"} className="BlogLinks"> Lexy's CodeCamp Survival Guide </Link>
+                </section>
                 </section>
                 <section>
                     <Footer />
