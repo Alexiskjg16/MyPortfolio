5a80c8715b40bd4649f742207c37383c76e13d74
Alexis K Grisham <alexisgrisham@Alexiss-MacBook-Air.local>
2018-10-15 20:54:55 -0400

    changedbackground

--- src/CodingFiles/HomeCoding.js
+++ src/CodingFiles/HomeCoding.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
 import {Link } from 'react-router-dom';
 import NavBar from '../NavBar';
-import Footer from '../FooterContact'
+import Footer from '../FooterContact';
+import scoreBoard from '../Pictures/scoreboard.png';
+import snowMan from '../Pictures/snowmangame.png';
+import weatherAPI from '../Pictures/weatherAPI.png';
 
 class HomeCoding extends Component {
     render() {
         return (
-            <div className="HomeCodePage">
+            <div>
                 <section>
                     <NavBar />
                 </section>
-                <ul>
-                    
-                    <li><a href="http://faithful-trouble.surge.sh/" target="_blank" rel="noopener noreferrer" className="CodeList">Go Jayhawks! </a>
-                        -- <a href="https://github.com/Alexiskjg16/VueScoreboard" target="_blank" rel="noopener noreferrer" className="CodeList">What's the Code?</a></li>
-                        <li><a href="https://snowing-hangman.surge.sh" target="_blank" rel="noopener noreferrer" className="CodeList"> Hangman </a>
-                        -- <a href="https://github.com/Alexiskjg16/SnowmanGame" target="_blank" rel="noopener noreferrer" className="CodeList">What's the Code?</a></li>
-                        <li><a href="https://weather-app.alexisgrisham.surge.sh" target="_blank" rel="noopener noreferrer" className="CodeList"> WeatherAPI </a>
-                        --<a href="https://github.com/Alexiskjg16/weather-app" target="_blank" rel="noopener noreferrer" className="CodeList"> What's the Code?</a></li>
-                        <li><a href="http://tbdvsupport.surge.sh/" target="_blank" rel="noopener noreferrer" className="CodeList">Capstone - TBDVSupport </a>
-                        --<a href="https://github.com/Alexiskjg16/THECapstoneProject" target="_blank" rel="noopener noreferrer" className="CodeList">What's the Code?</a></li>
-                        <li>Collaboration for "Suncoast Overflow" 
-                        --<a href="https://github.com/chris-packett/suncoast-overflow" target="_blank" rel="noopener noreferrer" className="CodeList">What's the Code?</a></li>
-                        <li className="CodeList"> Hey! This whole website is built using REACT.js from scratch. </li>
+                <ul className="HomeCodePage">
+                    <li>
+                    <img src={scoreBoard} alt="scoreboardImage" className="codeImages"/>
+                     <a href="http://faithful-trouble.surge.sh/" target="_blank" rel="noopener noreferrer" className="CodeList">Go Jayhawks! </a>
+                     -- <a href="https://github.com/Alexiskjg16/VueScoreboard" target="_blank" rel="noopener noreferrer" className="CodeList">What's the Code?</a></li>
+                    <li>
+                    <img src={snowMan} alt="SnowmanImage" className="codeImages" />
+                     <a href="https://snowing-hangman.surge.sh" target="_blank" rel="noopener noreferrer" className="CodeList"> Hangman </a>
+                     -- <a href="https://github.com/Alexiskjg16/SnowmanGame" target="_blank" rel="noopener noreferrer" className="CodeList">What's the Code?</a></li>
+                        </ul>
+                        <ul className="HomeCodePage">
+                        <li>
+                    <img src={weatherAPI} alt="weatherAPI" className="codeImages" />
+                     <a href="https://weather-app.alexisgrisham.surge.sh" target="_blank" rel="noopener noreferrer" className="CodeList"> WeatherAPI </a>
+                     --<a href="https://github.com/Alexiskjg16/weather-app" target="_blank" rel="noopener noreferrer" className="CodeList"> What's the Code?</a></li>
+                    <li>
+                     <a href="http://tbdvsupport.surge.sh/" target="_blank" rel="noopener noreferrer" className="CodeList">Capstone - TBDVSupport </a>
+                     --<a href="https://github.com/Alexiskjg16/THECapstoneProject" target="_blank" rel="noopener noreferrer" className="CodeList">What's the Code?</a></li>
+                    <li>Collaboration for "Suncoast Overflow" 
+                     --<a href="https://github.com/chris-packett/suncoast-overflow" target="_blank" rel="noopener noreferrer" className="CodeList">What's the Code?</a></li>
+                     <li className="CodeList"> Hey! This whole website is built using REACT.js from scratch. </li>
                 </ul>
                 <section>
                     <Footer />
