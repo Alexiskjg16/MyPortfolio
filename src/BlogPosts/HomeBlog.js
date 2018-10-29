eecdef9eb0fee8786a2c8d78c0d6f6ff01ff0d3b
Alexis K Grisham <alexisgrisham@Leenie.localdomain>
2018-10-29 13:57:25 -0400

    centeredblogpage

--- src/BlogPosts/HomeBlog.js
+++ src/BlogPosts/HomeBlog.js
@@ -1,48 +1,66 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import NavBar from '../NavBar';
-import Footer from '../FooterContact';
-import suncoastKids from '../Pictures/suncoastkids.jpg';
-import imposterSyndrome from '../Pictures/imposterSyndrome.jpeg';
-import classroomImage from '../Pictures/classroomImage.jpeg';
-import iCantEven from'../Pictures/I-Cant-Even.gif';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import NavBar from "../NavBar";
+import Footer from "../FooterContact";
+import suncoastKids from "../Pictures/suncoastkids.jpg";
+import imposterSyndrome from "../Pictures/imposterSyndrome.jpeg";
+import classroomImage from "../Pictures/classroomImage.jpeg";
+import iCantEven from "../Pictures/I-Cant-Even.gif";
 
 class HomeBlog extends Component {
-    render() {
-        return (
-            <div>
-                <section>
-                    < NavBar />
-                </section>
-                <h1 className="MainBlogTitle"> Blog Posts </h1>
-                <section className="MainBlogPage">
-                <section className="bloggerHeads">
-                <img src={suncoastKids} alt="GroupImage" className="blogImages" /> 
-                <Link to={"/BlogPosts/Sept518"} className="BlogLinks"> Being the Only Girl </Link>
-                </section>
-                <section className="bloggerHeads">
-                <img src={imposterSyndrome} alt="imposterSyndromeImage" className="blogImages" />
-                <Link to={"/BlogPosts/Sept1118"} className="BlogLinks"> The Imposter Syndrome </Link>
-                </section>
-                </section>
-                <section className="MainBlogPage">
-                <section className="bloggerHeads">
-                <img src={classroomImage} alt="classroomImage" className="blogImages" />
-                <Link to={"/BlogPosts/Sept2418"} className="BlogLinks"> Lexy's CodeCamp Survival Guide </Link>
-                </section>
-                <section className="bloggerHeads">
-                <img src={iCantEven} alt="ICan'tEven" className="blogImages" />
-                <Link to={"/BlogPosts/Oct2218"} className="BlogLinks"> I Can't Even! </Link>
-                </section>
-                </section>
-                <section>
-                    <Footer />
-                </section>
-            </div>
-
-        )
-    }
+  render() {
+    return (
+      <div>
+        <section>
+          <NavBar />
+        </section>
+        <h1 className="MainBlogTitle"> Blog Posts </h1>
+        <section className="MainBlogPage">
+          <section className="bloggerHeads">
+            <img src={suncoastKids} alt="GroupImage" className="onlyGirl" />
+            <Link to={"/BlogPosts/Sept518"} className="BlogLinks">
+              {" "}
+              Being the Only Girl{" "}
+            </Link>
+          </section>
+          <section className="bloggerHeads">
+            <img
+              src={imposterSyndrome}
+              alt="imposterSyndromeImage"
+              className="blogImages"
+            />
+            <Link to={"/BlogPosts/Sept1118"} className="BlogLinks">
+              {" "}
+              The Imposter Syndrome{" "}
+            </Link>
+          </section>
+        </section>
+        <section className="MainBlogPage">
+          <section className="bloggerHeads">
+            <img
+              src={classroomImage}
+              alt="classroomImage"
+              className="codeCamp"
+            />
+            <Link to={"/BlogPosts/Sept2418"} className="BlogLinks">
+              {" "}
+              Lexy's CodeCamp Survival Guide{" "}
+            </Link>
+          </section>
+          <section className="bloggerHeads">
+            <img src={iCantEven} alt="ICan'tEven" className="cantEven" />
+            <Link to={"/BlogPosts/Oct2218"} className="BlogLinks">
+              {" "}
+              I Can't Even!{" "}
+            </Link>
+          </section>
+        </section>
+        <section>
+          <Footer />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default HomeBlog;
--- src/FooterContact.js
+++ src/FooterContact.js
@@ -12,7 +12,7 @@ class Footer extends Component {
                 <section className="ContactMe">
                     <a href="https://twitter.com/Alexiskjg16" target="_blank" rel="noopener noreferrer"><img src={TwitterLogo} className="AddLogo" alt="Twitter" /></a>
                     <a href="https://www.linkedin.com/in/alexis-grisham-434401169/" target="_blank" rel="noopener noreferrer"><img src={LinkedInLogo} className="AddLogo" alt="LinkedIn" /></a>
-                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous"></link>
+                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossOrigin="anonymous"></link>
                     <a href="https://www.instagram.com/alexiskjg16/?hl=en" target="_blank" rel="noopener noreferrer"><img src={InstagramLogo} className="AddLogo" alt="Instagram" /></a>
                 </section>
                 <section>
